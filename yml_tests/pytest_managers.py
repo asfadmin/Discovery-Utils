6da42c60881ebab32719ee3e874580413d75df9d
Cameron Showalter <cjshowalter@alaska.edu>
2020-01-16 09:42:19 -0900

    Added 'pointer' method to the repair_wkt class

--- yml_tests/pytest_managers.py
+++ yml_tests/pytest_managers.py
@@ -1,4 +1,8 @@
+from pytest_WKTValidator import test_repair_wkt, test_files_to_wkt
 
 
-def test_WKTValidatorEndpoint(test_info, file_conf):
-	pass
+
+
+
+def test_WKTValidatorEndpoint(test_info, file_conf, cli_args):
+	test_repair_wkt(test_info, file_conf, cli_args)
