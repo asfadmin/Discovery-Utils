8c55de4fbdb5af56f5532cf4111c6f0786505619
Cameron Showalter <cjshowalter@alaska.edu>
2020-01-17 10:01:34 -0900

    Added manager for testing files_to_wkt

--- yml_tests/pytest_managers.py
+++ yml_tests/pytest_managers.py
@@ -6,3 +6,6 @@ from pytest_WKTValidator import test_repair_wkt, test_files_to_wkt
 
 def test_WKTValidatorEndpoint(test_info, file_conf, cli_args):
 	test_repair_wkt(test_info, file_conf, cli_args)
+
+def test_FilesToWKTEndpoint(test_info, file_conf, cli_args):
+	test_files_to_wkt(test_info, file_conf, cli_args)
