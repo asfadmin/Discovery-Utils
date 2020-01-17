dcd40af1f066af1b90478b867f4883d8be07f4e7
Cameron Showalter <cjshowalter@alaska.edu>
2020-01-17 13:41:28 -0900

    Added the 'test_vars' for variables declared in pytest_conf.yml

--- yml_tests/pytest_managers.py
+++ yml_tests/pytest_managers.py
@@ -4,8 +4,9 @@ from pytest_WKTValidator import test_repair_wkt, test_files_to_wkt
 
 
 
-def test_WKTValidatorEndpoint(test_info, file_conf, cli_args):
-	test_repair_wkt(test_info, file_conf, cli_args)
+def test_WKTValidatorEndpoint(test_info, file_conf, cli_args, test_vars):
+	test_repair_wkt(test_info, file_conf, cli_args, test_vars)
 
-def test_FilesToWKTEndpoint(test_info, file_conf, cli_args):
-	test_files_to_wkt(test_info, file_conf, cli_args)
+def test_FilesToWKTEndpoint(test_info, file_conf, cli_args, test_vars):
+	test_files_to_wkt(test_info, file_conf, cli_args, test_vars)
+
